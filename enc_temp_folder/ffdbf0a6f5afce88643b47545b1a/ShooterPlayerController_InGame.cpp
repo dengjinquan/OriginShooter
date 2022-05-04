dfa6119fa6eaef362b216856c661d58b755f1fef
dengjinquan <1542918678@qq.com>
2022-05-04 22:38:29 +0800

    Fix PlayerController

--- enc_temp_folder/ffdbf0a6f5afce88643b47545b1a/ShooterPlayerController_InGame.cpp
+++ enc_temp_folder/ffdbf0a6f5afce88643b47545b1a/ShooterPlayerController_InGame.cpp
@@ -12,10 +12,14 @@ void AShooterPlayerController_InGame::BeginPlay()
 
 void AShooterPlayerController_InGame::TickActor(float DeltaTime, enum ELevelTick TickType, FActorTickFunction& ThisTickFunction)
 {
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("%f,%f,%f"), RotationInput.Roll, RotationInput.Pitch, RotationInput.Yaw));
+
 	Super::TickActor(DeltaTime, TickType, ThisTickFunction);
 }
 
 void AShooterPlayerController_InGame::UpdateRotation(float DeltaTime)
 {
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, FString::Printf(TEXT("%f,%f,%f"), RotationInput.Roll, RotationInput.Pitch, RotationInput.Yaw));
+
 	Super::UpdateRotation(DeltaTime);
 }
