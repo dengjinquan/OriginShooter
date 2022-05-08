01509164b9dfa3e6fe8032e5de3bc7d618005feb
dengjinquan <1542918678@qq.com>
2022-05-08 22:51:43 +0800

    Test For Animation

---
+++ Source/OriginShooter/Private/GameFrame/Character/Animation/ShooterAnimInstance.cpp
@@ -0,0 +1,5 @@
+// Github Project For Fun
+
+
+#include "GameFrame/Character/Animation/ShooterAnimInstance.h"
+
---
+++ Source/OriginShooter/Public/GameFrame/Character/Animation/ShooterAnimInstance.h
@@ -0,0 +1,17 @@
+// Github Project For Fun
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORIGINSHOOTER_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
